375023f53916ed7650ab48c0a3ba37a0fece87f0
ian <ianfarr44@gmail.com>
2023-11-15 10:20:00 -0500

    handling input errors

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -1,17 +1,31 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
+  const usernameInputRef = React.useRef()
+  const [error, setError] = React.useState(null)
+
+  const handleChange = event => {
+    const input = event.target.value
+    const isValid = input === input.toLowerCase()
+    setError(isValid ? null : 'Username must be lower case')
+  }
+
   const handleSubmit = event => {
     event.preventDefault()
-    const input = event.target.elements[0].value
-    onSubmitUsername(input)
+    onSubmitUsername(usernameInputRef.current.value)
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="input">Username:</label>
-        <input type="text" />
+        <input
+          id="usernameInput"
+          type="text"
+          ref={usernameInputRef}
+          onChange={handleChange}
+        />
+        {error ? <p>{error}</p> : null}
       </div>
       <button type="submit">Submit</button>
     </form>
